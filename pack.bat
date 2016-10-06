1cc9431ee4e50b5c182095476109c407dbb1f0ee
raul naupari <raul.naupari@cignium.com>
2016-10-05 23:34:19 -0500

    tests

--- pack.bat
+++ pack.bat
@@ -1,3 +1,3 @@
-packages\NuGet.CommandLine.2.8.6\tools\nuget pack Jal.Finder\Jal.Finder.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Finder.Nuget
+packages\NuGet.CommandLine.3.4.4-rtm-final\tools\nuget pack Jal.Finder\Jal.Finder.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Finder.Nuget
 
 pause;
